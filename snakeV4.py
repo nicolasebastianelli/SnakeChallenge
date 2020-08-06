3a04b6ff43e3219d83566cdbaaba0c2cb9b861a5
Nicola <nicola.sebastianelli@studio.unibo.it>
2020-08-06 14:44:25 +0200

    Add files via upload

--- snakeV4.py
+++ snakeV4.py
@@ -114,38 +114,38 @@ class SnakeGraph:
         self.__adjacent_node = self.__compute_adjacent_nodes(edge_length)
         self.__found_paths = 0
 
-    # Build a list of list representing the adjacency matrix of the graph
+    # Build a list of list representing the adjacency matrix of the graph.
     def __compute_adjacent_nodes(self, edge_length):
 
         nodes = [[] for _ in range(self.__node_number)]
         for i in range(self.__node_number):
 
-            # Node is not in first row
+            # Node is not in first row.
             if i >= edge_length:
                 nodes[i].append(i - edge_length)
 
-            # Node is not in last row
+            # Node is not in last row.
             if i+edge_length < self.__node_number:
                 nodes[i].append(i + edge_length)
 
-            # Node is not in first column
+            # Node is not in first column.
             if (i % edge_length) != 0:
                 nodes[i].append(i - 1)
 
-            # Node is not in last column
+            # Node is not in last column.
             if ((i+1) % edge_length) != 0:
                 nodes[i].append(i + 1)
 
         return nodes
 
-    # Return the central node of odd length graphs, e.g. 5x5
+    # Return the central node of odd length graphs, e.g. 5x5.
     def __get_graph_central_node(self):
         if self.__node_number % 2 == 0:
             return None
         return int(self.__node_number/2)
 
     # Return the set of representative nodes for the current graph.
-    # A node is representative when applying rotations on the graph it covers other nodes of the graph
+    # A node is representative when applying rotations on the graph it covers other nodes of the graph.
     def __get_representative_subgraph_nodes(self):
         edge_length = int(self.__node_number**(1/2.))
         i = int(edge_length / 2)
@@ -153,10 +153,10 @@ class SnakeGraph:
         return [jj+ii*edge_length for ii in range(i) for jj in range(j)]
 
     # [Depth-First Search] Given a starting node and the visited nodes list,
-    # it recursively find all the Hamiltonian Paths
+    # it recursively find all the Hamiltonian Paths.
     def __find_hamiltonian_paths(self, start, visited):
 
-        # Stopping criteria if some nodes are unreachable
+        # Stopping criteria: if some nodes are unreachable stop the current exploration.
         if not self.__all_nodes_are_reachable(start, visited):
             return
 
@@ -169,11 +169,11 @@ class SnakeGraph:
         for adj in self.__adjacent_node[start]:
             if not visited[adj]:
                 self.__find_hamiltonian_paths(adj, visited)
-                visited[adj] = False  # Backtracking
+                visited[adj] = False  # Backtracking.
 
-    # [Flood-Fill Algorithm] To check if all nodes are still reachable
+    # [Flood-Fill Algorithm] Check if all nodes are still reachable.
     def __all_nodes_are_reachable(self, start, visited):
-        if visited.count(True) < 5:  # At least 5 true are required to create an island
+        if visited.count(True) < 5:  # At least 5 exploration are required to make a node unreachable.
             return True
         flooded_visited = visited[:]
         self.__flood_graph(start, flooded_visited)
@@ -182,7 +182,7 @@ class SnakeGraph:
         else:
             return False
 
-    # [Depth - First Search]
+    # [Depth-First Search]
     def __flood_graph(self, start, flooded_visited):
         flooded_visited[start] = True
         for adj in self.__adjacent_node[start]:
@@ -216,7 +216,7 @@ def path_count(edge_length):
 
 
 """
-Test run on: MacBook Pro - Core i5 2,3 GHz - Ram 8 GB 2133 MHz
+Tests run on: MacBook Pro - Core i5 2,3 GHz - Ram 8 GB 2133 MHz
 
 edge_length=1     time: 5.912e-05 seconds    solution: 1
 edge_length=2     time: 7.581e-05 seconds    solution: 8
@@ -224,10 +224,11 @@ edge_length=3     time: 3.850e-04 seconds    solution: 40
 edge_length=4     time: 7.284e-03 seconds    solution: 552
 edge_length=5     time: 0.3646438 seconds    solution: 8648
 edge_length=6     time: 24.621315 seconds    solution: 458696
+edge_length=7     time: 7505.7514 seconds    solution: 27070560
 """
 
 if __name__ == '__main__':
     import time
     start_time = time.time()
-    print(path_count(edge_length=6))
+    print(path_count(edge_length=7))
     print("--- %s seconds ---" % (time.time() - start_time))
